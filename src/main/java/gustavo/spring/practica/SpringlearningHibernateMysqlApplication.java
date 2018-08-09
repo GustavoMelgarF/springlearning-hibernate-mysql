7e7be53459e1b18265b5bd1b5f32b1e7ae73139c
gustavo <gustavo@gustavo.com>
2018-08-09 17:20:50 +0200

    initial

---
+++ src/main/java/gustavo/spring/practica/SpringlearningHibernateMysqlApplication.java
@@ -0,0 +1,12 @@
+package gustavo.spring.practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlearningHibernateMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlearningHibernateMysqlApplication.class, args);
+	}
+}
